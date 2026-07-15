aa6d4c08d34923d1639dc9a808c0e9ceac26fa78
hanhelong <hanhelong@example.com>
2026-07-15 01:24:28 +0000

    fix: 修复base路径为/delta-map/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/',
+  base: '/delta-map/',
   plugins: [
     react(),
     VitePWA({
@@ -18,8 +18,8 @@ export default defineConfig({
         background_color: '#0f172a',
         display: 'standalone',
         orientation: 'any',
-        start_url: '/',
-        scope: '/',
+        start_url: '/delta-map/',
+        scope: '/delta-map/',
         icons: [
           {
             src: 'pwa-192x192.png',
